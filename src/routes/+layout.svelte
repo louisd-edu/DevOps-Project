50e1e0224365bea33367499a2c584be561ba292b
Leonard Bauer <leochriba@gmail.com>
2025-10-09 18:30:35 +0100

    Implement authentication features with Supabase integration, including session management, login/signup forms, and error handling.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,34 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { invalidate } from '$app/navigation'
+ 	import { onMount } from 'svelte'
 	
-	let { children } = $props();
+
+	let { data, children } = $props()
+  	let { session, supabase } = $derived(data)
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+		if (newSession?.expires_at !== session?.expires_at) {
+			invalidate('supabase:auth')
+		}
+		})
+
+		return () => data.subscription.unsubscribe()
+	})
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<nav>
+	{#if session?.user}
+		<div>Log OUT</div>
+	{:else}
+		<div>Log IN</div>
+	{/if}
+</nav>
 {@render children?.()}
+
