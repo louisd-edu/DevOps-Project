6e32bd8fd193f3f320951411bc19c873ff1764e4
Leonard Bauer <leochriba@gmail.com>
2025-10-14 17:32:59 +0100

    Add user profile management and recipe display components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
-uaeua
+<script>
+
+    import RecipeComponent from "$lib/components/RecipeComponent.svelte";
+
+    let { data } = $props();
+  </script>
+
+  <div class="grid xl:grid-cols-3 gap-2 w-fit m-auto ">
+      {#each data.recipes as recipe (recipe.id)}
+          <RecipeComponent {recipe} />
+      {/each}
+  </div>
+
+
