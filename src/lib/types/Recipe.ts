efb4daf467a4e7a8222aefe204f21929e88a53c2
Leonard Bauer <leochriba@gmail.com>
2025-10-15 05:00:35 +0100

    Enhance recipe filtering and cuisine management with broader area support

--- src/lib/types/Recipe.ts
+++ src/lib/types/Recipe.ts
@@ -9,10 +9,10 @@ export type Recipe = {
     user_id: string
     recipename: string
     recipeimageurl: string | null
+    cuisine: string | null
+    cookingtime: number | null
     // the query in +page.server.ts requests a nested profiles object
     profiles: Profile
     recipeImage: string | null
     profileAvatar: string | null
-    cuisine: string | null
-    cookingtime: string | null
 }
