9521ed1aab4cae9bf873634e63afd084c6601ffc
Leonard Bauer <0bauel44@solent.ac.uk>
2025-12-10 14:46:21 +0000

    Logo Update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/yumlogo.svg';
 	import { invalidate } from '$app/navigation'
  	import { onMount, setContext } from 'svelte'
     import NavProfile from "$lib/components/NavProfile.svelte";
